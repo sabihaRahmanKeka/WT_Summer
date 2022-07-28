a25db6c7a18c2c23c871ecb1d2ad2e9f1d5b4187
sabihaRahmanKeka <106545263+sabihaRahmanKeka@users.noreply.github.com>
2022-07-28 18:33:11 +0600

    Add files via upload

---
+++ FTTask1/View/reg_page.php
@@ -0,0 +1,145 @@
+<html>
+    <title>Registration form </title>
+    <head>
+
+
+
+
+
+    
+        <link rel ="stylesheet" type="text/css" href = "../Css/mycss.css">
+        <div class = "header"><h1 align = "center">ABC Management System</h1></div>
+        <div class = "header2"><h3 align = "center">We create future</h3></div>
+
+
+        
+
+</head>
+
+
+</h1>
+
+<div class="sticky middlecolumn">
+
+    <div class="topnav">
+
+        <a href="#">Home</a>
+
+        <a href="#">About US</a>
+
+        <a href="#">Shop</a>
+
+    </div>
+
+    <div class = "regBx">
+    <h1>Registration Form</h1>
+</div>
+<br><br><br>
+</div
+
+    <body>
+        <form actiom="" method = "post" enctype="multipart/form-data">
+        
+            <table>
+           
+                <tr class = "box">
+                 <td>First Name</td>
+                    <td><input type="text" name = "fname"></td>
+                       
+</tr>
+
+
+
+<tr class = "box">
+    <td>Last Name</td>
+    <td><input type="text" name = "lname"></td>
+</tr>
+
+<tr class = "box">
+    <td>Age</td>
+    <td><input type="text" name = "age"></td>
+</tr>
+
+<tr class = "box">
+    <td>Destination</td>
+    <td><input type="radio" name = "dsg" value="Junior Programmer">Junior Programmer
+    <input type="radio" name = "dsg" value="Senior Programmer">Senior Programmer
+    <input type="radio" name = "dsg" value="Project Lead" >Project Lead</td>
+
+</tr>
+
+
+
+
+
+<tr class = "box">
+
+                    <td> Preferred Language</td>
+
+                    <td>
+
+                        <input type="checkbox" name="java" value="JAVA">JAVA
+
+                        <input type="checkbox" name="php" value="PHP">PHP
+
+                        <input type="checkbox" name="cpp" value="C++">C++
+
+                    </td>
+
+                </tr>
+
+              <tr class = "box">
+
+                    <td> Email</td>
+
+                    <td><input type="text" name="email"></td>
+
+                </tr>
+
+ 
+
+                <tr class = "box">
+
+                    <td>Password</td>
+
+                    <td><input type="password" name="pass"></td>
+
+                </tr>
+
+
+
+
+                <tr class = "box">
+
+                    <td>Please Choose a file</td>
+
+                    <td><input type="file" name="myfile"></td>
+
+                    <td>
+
+ 
+
+                    </td>
+
+                </tr>
+
+
+
+
+    
+<tr>
+            <td ><input  type="submit" class = "btn" name="submit" value="Submit">    
+
+            <input type="submit" class = "btn" name="reset" value="Reset"></td>
+
+       </tr>
+
+
+<tr>
+</tr>
+</div>
+</table>
+</div>
+</form> 
+</bodys>
+</html>
